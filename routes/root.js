b7997f90fb90ad30da044d142d9c8b66d5972bac
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2022-11-24 14:45:21 +0200

    002 added middlewares
    002 added middlewares

--- routes/root.js
+++ routes/root.js
@@ -2,13 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-//const hm = __dirname
-//const hm2 = path,join('views', 'index.html')
-
-//console.log(`hm ${hm}`)
-//console.log(`hm2 ${hm2}`)
-
-router.get('^/$|/index(.html)?', (req,res) => {
+router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
 
